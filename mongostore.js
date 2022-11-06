fcbf2a9bd90d57859c0d5bd9e7785c5cf143a568
angelator312 <angel.raev2012@gmail.com>
2022-11-06 08:25:51 +0200

    първо качване

---
+++ mongostore.js
@@ -0,0 +1,34 @@
+const { MongoClient } = require('mongodb');
+class Mongostore {
+    constructor(kolekcia) {
+        this.kolekcia = kolekcia
+    }
+    async conect(){
+        const client = new MongoClient('mongodb://test:test@127.0.0.1/test');
+        // Свързваме се със Mongo Сървъра
+        await client.connect();
+      
+        // Взимаме си базата с която ще работим
+        const database = client.db();
+        // Взимаме си колекцията с която ще работим
+        this.collection = database.collection(this.kolekcia);
+    }
+    async addkey(key, value) {
+        await this.collection.replaceOne({_id: key},{
+            _id:key,
+            ...value
+        },{upsert: true})
+    }
+    async deletekey(key) {
+        await this.collection.deleteOne({_id: key});
+
+    }
+    async getkey(key) {
+        const data= await this.collection.findOne({_id: key});
+        delete data._id;
+        return data
+    }
+}
+module.exports = {
+    Mongostore
+}
